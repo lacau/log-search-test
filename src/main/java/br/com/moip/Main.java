abf91ecf36a41849117b28117f7f9bfd1ef5c899
lacau <llacau@gmail.com>
2017-06-13 02:48:57 -0300

    Call  PrintService to show required outputs.

--- src/main/java/br/com/moip/Main.java
+++ src/main/java/br/com/moip/Main.java
@@ -1,35 +1,43 @@
 package br.com.moip;
 
+import java.util.List;
+import java.util.Map;
+
 import br.com.moip.model.FileResult;
 import br.com.moip.service.FileService;
+import br.com.moip.service.MathService;
+import br.com.moip.service.PrinterService;
 
 /**
  * Created by Lacau on 13/06/2017.
  */
 public class Main {
 
-    public static void main(String[] args) {
-        long init = System.currentTimeMillis();
-        //        if(args.length == 0) {
-        //            printHelp();
-        //            System.exit(0);
-        //        }
+    private static int MAX_RESULTS = 3;
 
-        final FileResult fileResult = FileService.getInstance().parseFile("C:\\Users\\Lacau\\Desktop\\moip\\log.txt");
-        if(!fileResult.hasData()) {
-            System.out.println("No data found! file: " + "C:\\Users\\Lacau\\Desktop\\moip\\log_min.txt");
+    public static void main(String[] args) {
+        //long init = System.currentTimeMillis();
+        if(args.length == 0) {
+            PrinterService.getInstance().printHelp();
             System.exit(0);
         }
-        System.out.println("URL COUNT: " + fileResult.getRequestURL().size());
-        System.out.println("STATUS COUNT: " + fileResult.getResponseStatus().size());
-        System.out.println("START: " + init);
-        System.out.println("END: " + System.currentTimeMillis());
-        System.out.println("ELAPSED: " + (System.currentTimeMillis() - init));
-    }
 
-    private static void printHelp() {
-        System.out.println("--- HELP ---");
-        System.out.println("command line -> 'java -jar log-server.jar <fileName>");
-        System.out.println("<fileName> -> log fileName with full path.");
+        final FileResult fileResult = FileService.getInstance().parseFile(args[0]);
+        final List<Map.Entry<String, Integer>> urls = MathService.getInstance().calculateMostCalledURLs(fileResult, MAX_RESULTS);
+        PrinterService.getInstance().printFormattedEntries(urls);
+
+        final List<Map.Entry<String, Integer>> status = MathService.getInstance().calculateResponseStatusCount(fileResult);
+        PrinterService.getInstance().printFormattedEntries(status);
+
+        if(!fileResult.hasData()) {
+            System.out.println("No data found! file: " + args[0]);
+            System.exit(0);
+        }
+
+        //        System.out.println("URL COUNT: " + fileResult.getRequestURL().size());
+        //        System.out.println("STATUS COUNT: " + fileResult.getResponseStatus().size());
+        //        System.out.println("START: " + init);
+        //        System.out.println("END: " + System.currentTimeMillis());
+        //        System.out.println("ELAPSED: " + (System.currentTimeMillis() - init));
     }
 }
