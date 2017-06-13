bccb9c17218712a4392bab778ca733d68a2e6379
lacau <llacau@gmail.com>
2017-06-13 00:23:50 -0300

    Show help when no parameter is informed through command line.

--- src/main/java/br/com/moip/Main.java
+++ src/main/java/br/com/moip/Main.java
@@ -6,6 +6,15 @@ package br.com.moip;
 public class Main {
 
     public static void main(String[] args) {
+        if(args.length == 0) {
+            printHelp();
+            System.exit(0);
+        }
+    }
 
+    private static void printHelp() {
+        System.out.println("--- HELP ---");
+        System.out.println("command line -> 'java -jar log-server.jar <fileName>");
+        System.out.println("<fileName> -> log fileName with full path.");
     }
 }
